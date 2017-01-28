64aec2bc982f3b22e23b79d4301b0ba48fbeb51a
Blake Merryman <blake@blackpixel.com>
2017-01-28 09:57:52 -0600

    Initial Commit

---
+++ SWAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SWAPI
+//
+//  Created by Blake Merryman on 1/28/17.
+//  Copyright © 2017 Blake Merryman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
