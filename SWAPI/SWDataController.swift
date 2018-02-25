e25b674fd8e732d368cd92b77abc23c16717f90f
Blake Merryman <blake.merryman@gmail.com>
2018-02-24 21:32:56 -0600

    Add correct date en/decoding strategy

--- SWAPI/SWDataController.swift
+++ SWAPI/SWDataController.swift
@@ -112,11 +112,29 @@ class SWDataController {
 
 }
 
+// MARK: - Private JSON Encoding/Decoding Helpers
+
+fileprivate extension DateFormatter {
+
+  /// Apple's implementation of ISO 8601 is incomplete.
+  /// - see: https://useyourloaf.com/blog/swift-codable-with-custom-dates/
+  static let iso8601Full: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+    formatter.calendar = Calendar(identifier: .iso8601)
+    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    formatter.locale = Locale(identifier: "en_US_POSIX")
+    return formatter
+  }()
+
+}
+
 fileprivate extension JSONDecoder {
 
   /// A shared JSON decoder to allow for easy decoding strategy adjustments.
   static let shared: JSONDecoder = {
     let decoder = JSONDecoder()
+    decoder.dateDecodingStrategy = .formatted(.iso8601Full)
     return decoder
   }()
 
@@ -127,6 +145,7 @@ fileprivate extension JSONEncoder {
   /// A shared JSON encoder to allow for easy decoding strategy adjustments.
   static let shared: JSONEncoder = {
     let encoder = JSONEncoder()
+    encoder.dateEncodingStrategy = .formatted(.iso8601Full)
     return encoder
   }()
 
