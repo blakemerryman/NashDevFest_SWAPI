dc0b94ec837575e2c6e00e268ca9b7b993b5c730
Blake Merryman <blake.merryman@gmail.com>
2018-02-24 21:13:43 -0600

    Update appearance

--- SWAPI/AppDelegate.swift
+++ SWAPI/AppDelegate.swift
@@ -34,14 +34,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
    */
   private func applyTheme() {
     UINavigationBar.appearance().barStyle = .black
-    UINavigationBar.appearance().tintColor = UIColor(red:0.18,
-                                                     green:0.8,
-                                                     blue:0.443,
-                                                     alpha:1.00)
-    UINavigationBar.appearance().barTintColor = UIColor(red:0.173,
-                                                        green:0.243,
-                                                        blue:0.314,
-                                                        alpha:1.00)
+    UINavigationBar.appearance().tintColor = .yellow
+    UINavigationBar.appearance().barTintColor = .black
     UINavigationBar.appearance().isTranslucent = false
     UILabel.appearance().font = UIFont.preferredFont(forTextStyle: .title1)
     UILabel.appearance().textColor = .white
